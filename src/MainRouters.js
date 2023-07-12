62448b92be849e2b905e67b856c0151261b2ee2e
Vladislav <Vladislav_lebedkin@mail.ru>
2023-07-12 21:19:59 +0300

    доделал все страницы меню и начал дополнять в базе данных

--- src/MainRouters.js
+++ src/MainRouters.js
@@ -1,11 +1,17 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home/Home";
 import Menu from "./pages/Menu/MenuMain";
 import Breakfast from "./pages/Menu/Breakfast";
 import Snacks from "./pages/Menu/Snacks";
 import Salad from "./pages/Menu/Salad";
 import Soups from "./pages/Menu/Soups";
+import Hotdishes from "./pages/Menu/Hotdishes";
+import Sidedishes from "./pages/Menu/Sidedishes";
+import Asia from "./pages/Menu/Asia";
+import Pizza from "./pages/Menu/Pizza";
+import Dessert from "./pages/Menu/Dessert";
+import Drink from "./pages/Menu/Drink";
 
 const MainRouters = () => {
   return (
@@ -16,6 +22,12 @@ const MainRouters = () => {
       <Route path="/menu/snacks" element={<Snacks />} />
       <Route path="/menu/salads" element={<Salad />} />
       <Route path="/menu/soup" element={<Soups />} />
+      <Route path="/menu/hotdishes" element={<Hotdishes />} />
+      <Route path="/menu/sidedishes" element={<Sidedishes />} />
+      <Route path="/menu/asia" element={<Asia />} />
+      <Route path="/menu/pizza" element={<Pizza />} />
+      <Route path="/menu/desserts" element={<Dessert />} />
+      <Route path="/menu/drinks" element={<Drink />} />
     </Routes>
   );
 };
---
+++ src/pages/Menu/Sidedishes.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { Footer } from '../../container';
+import { Navbar } from '../../components';
+import { images } from '../../constants';
+
+import Topbar from '../../components/Topbar/Topbar';
+import MenuItemMain from '../../components/Menuitem/MenuItemMain';
+
+const Sidedishes = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/sidedishes").then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
+  
+  return (
+    <div className="app__menu">
+        <Navbar />
+        <div className="app__menu-topbar">
+          <div className="topbar__video">
+            <div className="topbar__videoBackground">
+              <video preload="auto" playsInline autoPlay muted>
+                <source src={images.video} type="video/mp4" />
+              </video>
+            </div>
+          </div>
+          <Topbar data={data} />
+          <h3 className="app__menu-title">Гарниры</h3>
+          <div className="menuItem__cart">
+            {data.map((item) => (
+            <MenuItemMain data={item} key={item.id}/>
+          ))}
+          </div>
+        </div>
+        <Footer />
+      </div>
+  )
+}
+
+export default Sidedishes
